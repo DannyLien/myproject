91ffe1e45eab501d3a21e52e80a81f5a77cc6fa0
DannyLien <danny.lien1109@gmail.com>
2022-05-06 09:32:23 +0800

    6-3 FileReader(), 解決亂碼設定, BufferReader()

--- src/main/java/com/hank/io/Tester.java
+++ src/main/java/com/hank/io/Tester.java
@@ -1,12 +1,41 @@
 package com.hank.io;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
+import java.io.*;
 
 public class Tester {
     public static void main(String[] args) {
+        try {
+            BufferedReader br = new BufferedReader(new FileReader("data.txt"));
+            String line = br.readLine();
+            while (line != null) {
+                System.out.println(line);
+                line = br.readLine();
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+//        fileReader();
+//        inputStream();
+    }
+
+    private static void fileReader() {
+        try {
+            FileReader fr = new FileReader("data.txt");
+            int n = fr.read();
+            while (n != -1) {
+                System.out.print((char) n);
+                n = fr.read();
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void inputStream() {
         File file = new File("data.txt");
         System.out.println(file.exists());
         System.out.println(file.getAbsolutePath());
