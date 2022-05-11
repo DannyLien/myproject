27603de6d6a382b054405723c50ac49ff7da5e02
DannyLien <danny.lien1109@gmail.com>
2022-05-11 22:21:39 +0800

    7-3 Calendar, year, month, day, 三元運算式

--- src/main/java/com/hank/day/DayOfYear.java
+++ src/main/java/com/hank/day/DayOfYear.java
@@ -4,26 +4,34 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Calendar;
-import java.util.Scanner;
 
 public class DayOfYear {
+
     public static void main(String[] args) {
-//        Scanner scanner = new Scanner(System.in);
-//        int year = scanner.nextInt();
-        Calendar now = Calendar.getInstance();
-        int year = now.get(Calendar.YEAR);
+        Calendar calendar = Calendar.getInstance();
+        int year = calendar.get(Calendar.YEAR);
+        int month = calendar.get(Calendar.MONTH) + 1;
+        int day = calendar.get(Calendar.DAY_OF_MONTH);
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         try {
             System.out.println("Please enter year(2022) : ");
             String yearString = br.readLine();
-            if (yearString.length() > 0) {
-                year = Integer.parseInt(yearString);
-            }
+            year = (yearString.length() > 0) ? Integer.parseInt(yearString) : year;
             System.out.println(year);
 
+            System.out.println("Please enter month(5) : ");
+            String monthString = br.readLine();
+            month = (monthString.length() > 0) ? Integer.parseInt(monthString) : month;
+            System.out.println(month);
+
+            System.out.println("Please enter day(11) : ");
+            String dayString = br.readLine();
+            day = (dayString.length() > 0) ? Integer.parseInt(dayString) : day;
+            System.out.println(day);
+
+            System.out.println("You Enter Date : " + year + "/" + month + "/" + day);
         } catch (IOException e) {
             e.printStackTrace();
-
         }
     }
 }
