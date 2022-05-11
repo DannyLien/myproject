d9f333e68ba0bc2d5b1f29f102cd7c5d3080ef80
DannyLien <danny.lien1109@gmail.com>
2022-05-11 17:37:37 +0800

    7-2 Calendar and NumberFormatExecption

--- src/main/java/com/hank/day/DayOfYear.java
+++ src/main/java/com/hank/day/DayOfYear.java
@@ -3,17 +3,22 @@ package com.hank.day;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Calendar;
 import java.util.Scanner;
 
 public class DayOfYear {
     public static void main(String[] args) {
 //        Scanner scanner = new Scanner(System.in);
 //        int year = scanner.nextInt();
+        Calendar now = Calendar.getInstance();
+        int year = now.get(Calendar.YEAR);
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         try {
-            System.out.println("Please enter year : ");
+            System.out.println("Please enter year(2022) : ");
             String yearString = br.readLine();
-            int year = Integer.parseInt(yearString);
+            if (yearString.length() > 0) {
+                year = Integer.parseInt(yearString);
+            }
             System.out.println(year);
 
         } catch (IOException e) {
